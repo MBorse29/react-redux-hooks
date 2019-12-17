ca348e4f433f56f02408051d6bcf0a6aae123351
MBorse29 <manojborse29@gmail.com>
2019-12-17 15:13:30 +0530

    Comments added

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import configureStore from "./store";
 
+/* Implementation of Connect HOC */
+
 import Simple from "./components/Normal/Simple";
 import Counter from "./components/Normal/Counter";
 import Field from "./components/Normal/Field";
+
+/* Implementation of React-Redux Hooks */
 
 // import Simple from "./components/React-Hooks/Simple";
 // import Counter from "./components/React-Hooks/Counter";
